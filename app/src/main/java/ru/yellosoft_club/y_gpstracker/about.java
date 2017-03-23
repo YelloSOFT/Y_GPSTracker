83b83c3321b1d3a0cff482e6acbcf4de6dfe0801
YelloSOFT <yellosoft@mail.ru>
2017-03-23 17:29:42 +0300

    23.04.17 Исправлен перенос данных с первого активити на второй [2/2] Добавлен маркер + заданная позиция Попытка получить разрешение на отслеживание местоположения [1/2] Добавлен about*

---
+++ app/src/main/java/ru/yellosoft_club/y_gpstracker/about.java
@@ -0,0 +1,14 @@
+package ru.yellosoft_club.y_gpstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
